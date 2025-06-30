e02d5a4ac1e258b721d18c9acacbb7f240ff86d5
sarojsharma1 <sarojsharma.ce@gmail.com>
2025-06-30 08:39:47 +0545

    Extend Grpc method

--- microservices/data-service/src/main/java/com/architect/data_service/service/GrpcCall.java
+++ microservices/data-service/src/main/java/com/architect/data_service/service/GrpcCall.java
@@ -1,7 +1,19 @@
 package com.architect.data_service.service;
 
+import com.architect.common.grpc.HelloRequest;
+import com.architect.common.grpc.HelloResponse;
+import com.architect.common.grpc.HelloServiceGrpc;
+import io.grpc.stub.StreamObserver;
 import org.springframework.grpc.server.service.GrpcService;
 
 @GrpcService
-public class GrpcCall {
+public class GrpcCall extends HelloServiceGrpc.HelloServiceImplBase {
+
+    @Override
+    public void sayHello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+        String name = request.getName();
+        HelloResponse helloResponse = HelloResponse.newBuilder().setMessage(name).build();
+        responseObserver.onNext(helloResponse);
+        responseObserver.onCompleted();
+    }
 }
