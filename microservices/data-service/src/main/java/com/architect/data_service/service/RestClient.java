05b5a595ed5777ddb6879121fd02890aefb73777
sarojsharma1 <sarojsharma.ce@gmail.com>
2025-07-02 20:17:35 +0545

    Use service id from config file

--- microservices/data-service/src/main/java/com/architect/data_service/service/RestClient.java
+++ microservices/data-service/src/main/java/com/architect/data_service/service/RestClient.java
@@ -1,14 +1,15 @@
 package com.architect.data_service.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class RestClient {
     private final WebClient webClient;
-
-    public RestClient(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder.baseUrl("http://process-service")
+    public RestClient(@Value("${process.service.baseUrl}") String processServiceBaseURL,
+                      WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl(processServiceBaseURL)
                 .build();
     }
 
