53378dd38661ce2daec5d33068a2c53e82f34415
sarojsharma1 <sarojsharma.ce@gmail.com>
2025-07-08 17:20:19 +0545

    Add async operation

--- microservices/data-service/src/main/java/com/architect/data_service/DataServiceApplication.java
+++ microservices/data-service/src/main/java/com/architect/data_service/DataServiceApplication.java
@@ -3,9 +3,11 @@ package com.architect.data_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableAsync
 public class DataServiceApplication {
 
 	public static void main(String[] args) {
--- microservices/data-service/src/main/java/com/architect/data_service/service/RestClient.java
+++ microservices/data-service/src/main/java/com/architect/data_service/service/RestClient.java
@@ -3,9 +3,12 @@ package com.architect.data_service.service;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class RestClient {
@@ -30,7 +33,21 @@ public class RestClient {
                 });
     }
 
-    public void fallbackHandler(Throwable t) {
+    public Flux<String> fallbackHandler(Throwable t) {
         System.out.println("Unable to connect to service " + t.getMessage());
+        return Flux.just("Fallback response");
+    }
+
+    @Async
+    public void asyncOp() {
+        System.out.println("Hello");
+    }
+
+    public void customAsyncOp() {
+        CompletableFuture<String> res = CompletableFuture.supplyAsync(() -> {
+            return "success";
+        }).exceptionally(ex -> {
+            return "error";
+        });
     }
 }
