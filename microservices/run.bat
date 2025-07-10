75ae02d7bf58d45713cd26bb37335d03f9176825
sarojsharma1 <sarojsharma.ce@gmail.com>
2025-07-10 15:30:20 +0545

    Remove jenkins from docker compose

--- microservices/run.bat
+++ microservices/run.bat
@@ -1,20 +1,16 @@
 cd common-lib
-mvn clean install -DskipTests
-cd ..
+mvn clean install
 
 cd discovery-server
-mvn clean package -DskipTests
-start "Discovery-Server" cmd /k java -jar target\discovery-server-0.0.1-SNAPSHOT.jar --spring.profiles.active=default
+mvn clean package
+java -jar target\discovery-server-0.0.1-SNAPSHOT.jar --spring.profiles.active=default
 cd ..
 
 cd config-server
-mvn clean package -DskipTests
-start "Config-Server" cmd /k java -jar target\config-server-0.0.1-SNAPSHOT.jar --spring.profiles.active=default
+mvn clean package
+java -jar target\config-server-0.0.1-SNAPSHOT.jar --spring.profiles.active=default
 cd ..
 
 cd api-gateway
-mvn clean package -DskipTests
-start "Api-Gateway" cmd /k java -jar target\api-gateway-0.0.1-SNAPSHOT.jar --spring.profiles.active=default
-cd ..
-
-pause
+mvn clean package
+java -jar target\api-gateway-0.0.1-SNAPSHOT.jar --spring.profiles.active=default
