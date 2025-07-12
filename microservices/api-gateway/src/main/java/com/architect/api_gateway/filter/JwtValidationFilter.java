e836b9183df6b5c1b6df872eeb4ddf83f471bea2
sarojsharma1 <sarojsharma.ce@gmail.com>
2025-07-12 16:55:33 +0545

    Add required dependency for auth service

---
+++ microservices/api-gateway/src/main/java/com/architect/api_gateway/filter/JwtValidationFilter.java
@@ -0,0 +1,13 @@
+package com.architect.api_gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+public class JwtValidationFilter implements GlobalFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return null;
+    }
+}
