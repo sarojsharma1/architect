52788e32c6fac15b3d04a9425f9bdfad472b1556
sarojsharma1 <sarojsharma.ce@gmail.com>
2025-07-16 10:37:26 +0545

    Add exception handler

---
+++ microservices/common-lib/src/main/java/com/architect/common_lib/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.architect.common_lib.exception;
+
+import com.architect.common_lib.dto.ResponseDto;
+import jakarta.ws.rs.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(value = NotFoundException.class)
+    public ResponseEntity<ResponseDto> handleNotFoundException(NotFoundException ex, WebRequest request) {
+        ResponseDto responseDto = new ResponseDto(ex.getMessage(), null);
+        return new ResponseEntity<>(responseDto, HttpStatus.NOT_FOUND);
+    }
+}
