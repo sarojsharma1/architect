e9e65bbeaf0464c514caa6c70ed7235e29690c07
sarojsharma1 <sarojsharma.ce@gmail.com>
2025-07-26 21:39:22 +0545

    Add actuator for health check

--- microservices/api-gateway/src/main/java/com/architect/api_gateway/config/SecurityConfig.java
+++ microservices/api-gateway/src/main/java/com/architect/api_gateway/config/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http.authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/auth/**", "/data/**", "/process/**").permitAll()
+                        .pathMatchers("/auth/**", "/data/**", "/process/**", "/actuator/**").permitAll()
                         .anyExchange().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(Customizer.withDefaults()))
