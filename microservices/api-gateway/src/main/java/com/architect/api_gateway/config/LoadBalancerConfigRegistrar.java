6310ce32686080c6d71d81e1377e679593cc06b1
sarojsharma1 <sarojsharma.ce@gmail.com>
2025-07-30 14:47:36 +0545

    Delete load balancer

--- microservices/api-gateway/src/main/java/com/architect/api_gateway/config/LoadBalancerConfigRegistrar.java
+++
@@ -1,9 +0,0 @@
-package com.architect.api_gateway.config;
-
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@LoadBalancerClient(name = "data-service", configuration = CustomLoadBalancerConfig.class)
-public class LoadBalancerConfigRegistrar {
-}
